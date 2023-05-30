a69a08b373e76c31567ccbe7eaf4e40614c537d8
eltar14 <lebouant2@gmail.com>
2023-05-30 23:50:30 +0200

    jmerise + sql creation tables

---
+++ spotify_creation.sql
@@ -0,0 +1,116 @@
+------------------------------------------------------------
+--        Script Postgre 
+------------------------------------------------------------
+
+
+
+------------------------------------------------------------
+-- Table: user
+------------------------------------------------------------
+CREATE TABLE public.user(
+	id_user          SERIAL NOT NULL ,
+	name_user        VARCHAR (120) NOT NULL ,
+	surname_user     VARCHAR (120) NOT NULL ,
+	birthdate_user   DATE  NOT NULL ,
+	password_user    VARCHAR (120) NOT NULL ,
+	email_user       VARCHAR (120) NOT NULL  ,
+	CONSTRAINT user_PK PRIMARY KEY (id_user)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: playlist
+------------------------------------------------------------
+CREATE TABLE public.playlist(
+	id_playlist      SERIAL NOT NULL ,
+	name_playlist    VARCHAR (120) NOT NULL ,
+	cover_playlist   VARCHAR (480) NOT NULL  ,
+	CONSTRAINT playlist_PK PRIMARY KEY (id_playlist)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: artist
+------------------------------------------------------------
+CREATE TABLE public.artist(
+	id_artist            SERIAL NOT NULL ,
+	name_artist          VARCHAR (120) NOT NULL ,
+	description_artist   VARCHAR (2000)  NOT NULL ,
+	type_artist          VARCHAR (120) NOT NULL  ,
+	CONSTRAINT artist_PK PRIMARY KEY (id_artist)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: album
+------------------------------------------------------------
+CREATE TABLE public.album(
+	id_album      SERIAL NOT NULL ,
+	name_album    VARCHAR (120) NOT NULL ,
+	date_album    DATE  NOT NULL ,
+	cover_album   VARCHAR (480) NOT NULL ,
+	style_album   VARCHAR (120) NOT NULL ,
+	id_artist     INT  NOT NULL  ,
+	CONSTRAINT album_PK PRIMARY KEY (id_album)
+
+	,CONSTRAINT album_artist_FK FOREIGN KEY (id_artist) REFERENCES public.artist(id_artist)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: song
+------------------------------------------------------------
+CREATE TABLE public.song(
+	id_song         SERIAL NOT NULL ,
+	title_song      VARCHAR (120) NOT NULL ,
+	duration_song   INT  NOT NULL ,
+	link_song       VARCHAR (480) NOT NULL ,
+	id_album        INT  NOT NULL  ,
+	CONSTRAINT song_PK PRIMARY KEY (id_song)
+
+	,CONSTRAINT song_album_FK FOREIGN KEY (id_album) REFERENCES public.album(id_album)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: user_playlist
+------------------------------------------------------------
+CREATE TABLE public.user_playlist(
+	id_playlist     INT  NOT NULL ,
+	id_user         INT  NOT NULL ,
+	date_playlist   DATE  NOT NULL  ,
+	CONSTRAINT user_playlist_PK PRIMARY KEY (id_playlist,id_user)
+
+	,CONSTRAINT user_playlist_playlist_FK FOREIGN KEY (id_playlist) REFERENCES public.playlist(id_playlist)
+	,CONSTRAINT user_playlist_user0_FK FOREIGN KEY (id_user) REFERENCES public.user(id_user)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: playlist_song
+------------------------------------------------------------
+CREATE TABLE public.playlist_song(
+	id_playlist              INT  NOT NULL ,
+	id_song                  INT  NOT NULL ,
+	date_add_song_playlist   DATE  NOT NULL  ,
+	CONSTRAINT playlist_song_PK PRIMARY KEY (id_playlist,id_song)
+
+	,CONSTRAINT playlist_song_playlist_FK FOREIGN KEY (id_playlist) REFERENCES public.playlist(id_playlist)
+	,CONSTRAINT playlist_song_song0_FK FOREIGN KEY (id_song) REFERENCES public.song(id_song)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: history
+------------------------------------------------------------
+CREATE TABLE public.history(
+	id_song   INT  NOT NULL ,
+	id_user   INT  NOT NULL  ,
+	CONSTRAINT history_PK PRIMARY KEY (id_song,id_user)
+
+	,CONSTRAINT history_song_FK FOREIGN KEY (id_song) REFERENCES public.song(id_song)
+	,CONSTRAINT history_user0_FK FOREIGN KEY (id_user) REFERENCES public.user(id_user)
+)WITHOUT OIDS;
+
+
+

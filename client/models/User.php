7469875480e01ea94c42e932528e6ef3291285dd
eltar14 <lebouant2@gmail.com>
2023-05-30 16:27:24 +0200

    db

---
+++ client/models/User.php
@@ -0,0 +1,7 @@
+<?php
+
+class User{
+static function getName($id_user){
+    #todo
+}
+}
---
+++ constants.php
@@ -0,0 +1,8 @@
+<?php
+
+// Database constants.
+const DB_USER = 'groupe11';
+const DB_PASSWORD = 'Groupe11';
+const DB_NAME = 'groupe11';
+const DB_SERVER = '127.0.0.1';
+const DB_PORT = '5432';

17a0465814dbc85af90fe28f5c87ba38516482ca
eltar14 <lebouant2@gmail.com>
2023-05-31 16:02:13 +0200

    modif accueil.php

--- client/accueil.php
+++ client/accueil.php
@@ -7,21 +7,13 @@ $auth = auth_verif();
 
 switch ($auth) {
     case "connected":
-
-        require_once "nav.php";
-        // require once fichier html
-        echo "<h1>CONNECTED</h1>";
-        //require_once "../views/footer.php";
         header("Location: accueil.html");
-
         break;
 
     case "incorrect":
-        echo "incorrect !";
         $displayConnectionError = "";
 
     default:
-        echo "default";
         require_once "connexion.php";
         break;
 }

af6c4a8c65fad49230a85d0bd0528d830ac7b194
eltar14 <lebouant2@gmail.com>
2023-05-31 10:49:43 +0200

    méthodes get user

--- client/php/User.php
+++ client/php/User.php
@@ -2,5 +2,51 @@
 require_once "../../DB.php";
 class User
 {
+    /**
+     * retourne le nom de l'utilisateur dont l'id est passé en argument
+     * @param $id_user
+     * @return mixed
+     */
+    static function getName($id_user){
+        $db = DB::connexion();
+        $request = 'SELECT name_user FROM "user" WHERE id_user=:id_user;';
+        $statement = $db->prepare($request);
+        $statement->bindParam(':id_user', $id_user);
+        $statement->execute();
 
+        return $statement->fetch()[0];
+    }
+
+
+    static function getSurname($id_user){
+        $db = DB::connexion();
+        $request = 'SELECT surname_user FROM "user" WHERE id_user=:id_user;';
+        $statement = $db->prepare($request);
+        $statement->bindParam(':id_user', $id_user);
+        $statement->execute();
+
+        return $statement->fetch()[0];
+    }
+
+
+    static function getBirthdate($id_user){
+        $db = DB::connexion();
+        $request = 'SELECT birthdate_user FROM "user" WHERE id_user=:id_user;';
+        $statement = $db->prepare($request);
+        $statement->bindParam(':id_user', $id_user);
+        $statement->execute();
+
+        return $statement->fetch()[0];
+    }
+
+
+    static function getEmail($id_user){
+        $db = DB::connexion();
+        $request = 'SELECT email_user FROM "user" WHERE id_user=:id_user;';
+        $statement = $db->prepare($request);
+        $statement->bindParam(':id_user', $id_user);
+        $statement->execute();
+
+        return $statement->fetch()[0];
+    }
 }

b4187b4641c15e10585d771ba9ee909fd3b4c447
Quentin Le Goff <quentin.le-goff@isen-ouest.yncrea.fr>
2023-06-01 17:31:32 +0200

    song commited

--- client/php/Song.php
+++ client/php/Song.php
@@ -32,4 +32,23 @@ class Song
         return $statement->fetch()[0];
     }
 
+    static function getLink($idSong){
+        $db = DB::connexion();
+        $request = 'SELECT link_song FROM song WHERE id_song=:id_song;';
+        $statement = $db->prepare($request);
+        $statement->bindParam(':id_song', $idSong);
+        $statement->execute();
+
+        return $statement->fetch()[0];
+    }
+
+    static function getSong($idSong){
+        $db = DB::connexion();
+        $request = 'SELECT * FROM song WHERE id_song=:id_song;';
+        $statement = $db->prepare($request);
+        $statement->bindParam(':id_song', $idSong);
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
 }

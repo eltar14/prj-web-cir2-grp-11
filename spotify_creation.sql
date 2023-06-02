6742bb05b06d6f698d05d309ec9a64cfc539c7f5
mpaitier <mathias.paitier@isen-ouest.yncrea.fr>
2023-06-02 09:43:42 +0200

    mauvais mail pour gou

--- spotify_creation.sql
+++ spotify_creation.sql
@@ -158,7 +158,7 @@ CREATE TABLE public.history(
 
 /* Ajout d'utilisateurs */
 
-INSERT INTO "user"(surname_user, name_user, birthdate_user, password_user, email_user) values ('Sermon-Thuillier', 'Goustan', '2002-11-12', crypt('gou', gen_salt('md5')) , 'goustan.sermon-thuiller@isen.fr');
+INSERT INTO "user"(surname_user, name_user, birthdate_user, password_user, email_user) values ('Sermon-Thuillier', 'Goustan', '2002-11-12', crypt('gou', gen_salt('md5')) , 'goustan.sermon-thuillier@isen.fr');
 INSERT INTO "user"(surname_user, name_user, birthdate_user, password_user, email_user) values ('Paul', 'Fontaine', '2003-06-03', crypt('paul', gen_salt('md5')) , 'paul.fontaine@isen.fr');
 INSERT INTO "user"(surname_user, name_user, birthdate_user, password_user, email_user) values ('Paitier', 'Mathias', '2003-05-12', crypt('mat', gen_salt('md5')) , 'mathias.paitier@isen.fr');
 INSERT INTO "user"(surname_user, name_user, birthdate_user, password_user, email_user) values ('Le Goff', 'Quentin', '2003-02-28', crypt('que', gen_salt('md5')) , 'que.legoff@isen.fr');
